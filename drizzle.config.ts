837fa3ce685f2944ff35b78b0bbee3b2877cc8fd
Karan <karanhengade9595@gmail.com>
2024-12-07 22:33:16 +0530

    drizzle bugfixes

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+
+export default {
+  dialect: "postgresql", // Add this line
+  driver: "pg", // Ensure this is correct for your setup
+  schema: "./utils/schema.jsx",
+  dbCredentials: {
+    connectionString: process.env.NEXT_PUBLIC_DATABASE_URL,
+  },
+} satisfies Config;
--- utils/schema.jsx
+++ utils/schema.jsx
@@ -1,11 +1,9 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-
+import { pgTable, varchar, serial } from "drizzle-orm/pg-core";
 
 export const Budgets = pgTable('budgets', {
     id: serial('id').primaryKey(),
     name: varchar('name').notNull(),
     amount: varchar('amount').notNull(),
     icon: varchar('icon'),
-    createdBy: varchar('createdBy').notNull()
-
-})
+    createdBy: varchar('createdBy').notNull(),
+});
