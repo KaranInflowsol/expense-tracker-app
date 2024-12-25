917b6c179b3b423af328bb8220d383e021806c38
Karan <karanhengade9595@gmail.com>
2024-12-25 22:38:22 +0530

    worked on create budget form

--- app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -4,7 +4,9 @@ import CreateBudget from './CreateBudget'
 function BudgetList() {
     return (
         <div className='mt-7'>
-            <CreateBudget />
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                <CreateBudget />
+            </div>
         </div>
     )
 }
