dd3278344e021831e23f2e53a7fbfcd80ff6d73b
Karan <karanhengade4636@gmail.com>
2025-02-20 13:05:50 +0530

    display expenses is completed

--- utils/schema.jsx
+++ utils/schema.jsx
@@ -1,9 +1,17 @@
-import { pgTable, varchar, serial, numeric } from "drizzle-orm/pg-core";
+import { pgTable, varchar, serial, numeric, integer } from "drizzle-orm/pg-core";
 
 export const Budgets = pgTable('budgets', {
     id: serial('id').primaryKey(),
     name: varchar('name').notNull(),
-    amount: numeric('amount').notNull(), // Use numeric for amounts
+    amount: varchar('amount').notNull(), // Use numeric for amounts
     icon: varchar('icon'),
     createdBy: varchar('createdBy').notNull(),
 });
+
+export const Expenses= pgTable('expenses',{
+    id:serial('id').primaryKey(),
+    name:varchar('name').notNull(),
+    amount:numeric('amount').notNull().default(0),
+    budgetId:integer('budgetId').references(()=>Budgets.id),
+    createdAt:varchar('createdAt').notNull()
+})
